d6b45524fe22fad338a790b1f2b45e29aac9bb9f
kor-901 <52075363+kor-901@users.noreply.github.com>
2019-07-17 21:59:34 +0900

    Add unique constraint to name column

--- db/migrate/20190717124411_devise_create_users.rb
+++ db/migrate/20190717124411_devise_create_users.rb
@@ -41,6 +41,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
-    add_index :users, :name
+    add_index :users, :name,                 unique: true
   end
 end
