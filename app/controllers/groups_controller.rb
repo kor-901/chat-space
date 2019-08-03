caa0d99d4ea59875ed7dcc2918692bcaa7df05b1
kor-901 <52075363+kor-901@users.noreply.github.com>
2019-08-03 16:44:00 +0900

    Add function of create group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,11 +5,21 @@ class GroupsController < ApplicationController
 	end
 
 	def create
+		if Group.create(group_params)
+			redirect_to root_path
+		else
+			render :new
+		end
 	end
 
 	def edit
 	end
 
 	def update
+	end
+
+	private
+	def group_params
+		params.require(:group).permit(:name, {user_ids: []})
 	end
 end
