aa9732b546c9af7bb76af352c510fb02af3994e7
kor-901 <52075363+kor-901@users.noreply.github.com>
2019-08-04 14:36:01 +0900

    Apply set_group method to before_action on edit and update action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+	before_action :set_group, only: [:edit, :update]
+
 	def index
 	end
 
@@ -16,12 +18,7 @@ class GroupsController < ApplicationController
 		end
 	end
 
-	def edit
-		@group = Group.find(params[:id])
-	end
-
 	def update
-		@group = Group.find(params[:id])
 		if @group.update(group_params)
 			redirect_to root_path, notice: "グループを編集しました"
 		else
