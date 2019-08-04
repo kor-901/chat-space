9ada0fd83094a125d4fb24c23fec43557faa84b0
kor-901 <52075363+kor-901@users.noreply.github.com>
2019-08-04 14:27:32 +0900

    Define method of selecting group instance

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -33,4 +33,8 @@ class GroupsController < ApplicationController
 	def group_params
 		params.require(:group).permit(:name, {user_ids: []})
 	end
+
+	def set_group
+		@group = Group.find(params[:id])
+	end
 end
