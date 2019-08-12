8686695251b5c8afb70370ef8b73914dff47f247
kor-901 <52075363+kor-901@users.noreply.github.com>
2019-08-13 00:34:24 +0900

    Add test code about messages controller's create action

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -37,4 +37,55 @@ describe MessagesController do
 
 	end
 
+	describe 'POST #create' do
+		let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
+
+		context 'log in' do
+			before do
+				login user
+			end
+
+			context "can save" do
+				subject {post :create, params: params}
+
+				it "count up message" do
+					# message = build(:message)
+					# expect(message.save).to be_truthy
+					expect{subject}.to change(Message, :count).by(1)
+				end
+
+				it "redirect to group_messages_path" do
+					subject
+					expect(response).to redirect_to(group_messages_path(group))
+				end
+			end
+
+			context 'can not save' do
+				let(:invalid_params) {{group_id: group.id, user_id: user.id, message: attributes_for(:message, text: nil, image: nil)}}
+				subject {post :create, params: invalid_params}
+
+				it "does not count up" do
+					# message = Message.new
+					# expect(message.save).to be_falsey
+					expect{subject}.not_to change(Message, :count)
+				end
+	
+				it "renders the :index template" do
+					subject
+					expect(response).to render_template :index
+				end
+			end
+
+		end
+
+		context 'not log in' do
+
+			it "redirect to new_user_session_path" do
+				post :create, params: params
+				expect(response).to redirect_to(new_user_session_path)
+			end
+
+		end
+	end
+
 end
