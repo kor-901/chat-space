e7eed3a002383fb56000cf8c37d4f58a317e837d
kor-901 <52075363+kor-901@users.noreply.github.com>
2019-08-12 18:26:39 +0900

    Create spce file of message model

---
+++ spec/models/message_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe Message do
+	describe '#create' do
+
+		it "is valid with a text" do
+		end
+
+		it "is valid with a image" do
+		end
+
+		it "is valid with a text, image" do
+		end
+
+		it "is invalid without a text, image" do
+		end
+
+		it "is invalid without a group_id" do
+		end
+
+		it "is invalid without a user_id" do
+		end
+
+	end
+end
