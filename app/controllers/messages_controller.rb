e1061f322cc5b82777ab5033b4c13bbced177b6b
kor-901 <52075363+kor-901@users.noreply.github.com>
2019-08-12 23:37:26 +0900

    Modify message index view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
 	before_action :set_group
 
 	def index
+		@message = Message.new
 		@messages = @group.messages.includes(:user).order("created_at")
 	end
 
