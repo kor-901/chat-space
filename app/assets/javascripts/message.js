1de0716f70a3a9e718a0a629227885e217168383
kor-901 <52075363+kor-901@users.noreply.github.com>
2019-08-15 15:33:01 +0900

    Append HTML about message text

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,23 @@
 $(function() {
+	function buildStartHTML(data) {
+		var html = `<div class="message">
+									<div class="message__title">
+										<p class="message__title__username">${data.user_name}</p>
+										<p class="message__title__timestamp">${data.created_at}</p>
+									</div>`;
+		return html;
+	};
+
+	function buildTextHTML(text) {
+		var html = `	<p class="message__text">${text}</p>`;
+		return html;
+	};
+
+	function buildEndHTML() {
+		var html = '</div>';
+		return html;
+	};
+
 	$('.input-form').on('submit', function(e){
 		e.preventDefault();
 		var formData = new FormData(this);
@@ -13,7 +32,13 @@ $(function() {
 			contentType: false
 		})
 		.done(function(data){
-			
+			var htmlArray = new Array(buildStartHTML(data));
+			if (data.text !== "") {
+				htmlArray.push(buildTextHTML(data.text));
+			}
+			htmlArray.push(buildEndHTML());
+			var html = htmlArray.join('\n');
+			$('.messages').append(html);
 		})
 		.fail(function(){
 
