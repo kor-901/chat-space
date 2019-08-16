1b322047b07a12e8152357b86aa7d285d33d7f74
kor-901 <52075363+kor-901@users.noreply.github.com>
2019-08-16 14:59:35 +0900

    Append HTML about user search result

--- app/assets/javascripts/search_users.js
+++ app/assets/javascripts/search_users.js
@@ -1,12 +1,29 @@
 $(function() {
+	function appendSearchResult(user) {
+		var html = `<div class="chat-group-user clearfix">
+									<p class="chat-group-user__name">${user.name}</p>
+									<div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.user_id}" data-user-name="${user.name}">追加</div>
+								</div>`;
+		$('#user-search-result').append(html);
+	};
+
 	$('#user-search-field').on('keyup', function(){
 		var input = $('#user-search-field').val();
-		
+
 		$.ajax({
 			type: 'GET',
 			url: '/users',
 			data: {keyword: input},
 			dataType: 'json'
+		})
+		.done(function(users){
+			$('#user-search-result').empty();
+			
+			if (users.length !==0) {
+				users.forEach(function(user) {
+					appendSearchResult(user);
+				});
+			};
 		});
 	});
 });
